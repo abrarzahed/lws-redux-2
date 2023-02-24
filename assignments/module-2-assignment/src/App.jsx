495047a3ae6800e4b4d967c1613be15da0e9d7e1
abrarzahed <abrarzahed6986@gmail.com>
2023-02-24 12:19:56 +0600

    converted html to react jsx

---
+++ assignments/module-2-assignment/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Preview from "./components/Preview";
+import TopForm from "./components/TopForm";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TopForm />
+      <Preview />
+    </>
+  );
+}
+
+export default App;
---
+++ assignments/module-2-assignment/src/components/TopForm.jsx
@@ -0,0 +1,124 @@
+import frameImg from "../assets/img/icons/Frame.svg";
+import classImg from "../assets/img/icons/class.svg";
+import guestsImg from "../assets/img/icons/guests.svg";
+
+export default function TopForm() {
+  return (
+    <div className="mt-[160px] mx-4 md:mt-[160px] relative">
+      <div className="bg-white rounded-md max-w-6xl w-full mx-auto">
+        <form className="first-hero lws-inputform">
+          <div className="des-from">
+            <p>Destination From</p>
+            <div className="flex flex-row">
+              <img src={frameImg} alt="" />
+              <select
+                className="outline-none px-2 py-2 w-full"
+                name="from"
+                id="lws-from"
+                required
+              >
+                <option value="" hidden>
+                  Please Select
+                </option>
+                <option>Dhaka</option>
+                <option>Sylhet</option>
+                <option>Saidpur</option>
+                <option>Cox's Bazar</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="des-from">
+            <p>Destination To</p>
+            <div className="flex flex-row">
+              <img src={frameImg} alt="" />
+              <select
+                className="outline-none px-2 py-2 w-full"
+                name="to"
+                id="lws-to"
+                required
+              >
+                <option value="" hidden>
+                  Please Select
+                </option>
+                <option>Dhaka</option>
+                <option>Sylhet</option>
+                <option>Saidpur</option>
+                <option>Cox's Bazar</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="des-from">
+            <p>Journey Date</p>
+            <input
+              type="date"
+              className="outline-none px-2 py-2 w-full date"
+              name="date"
+              id="lws-date"
+              required
+            />
+          </div>
+
+          <div className="des-from">
+            <p>Guests</p>
+            <div className="flex flex-row">
+              <img src={guestsImg} alt="" />
+              <select
+                className="outline-none px-2 py-2 w-full"
+                name="guests"
+                id="lws-guests"
+                required
+              >
+                <option value="" hidden>
+                  Please Select
+                </option>
+                <option value="1">1 Person</option>
+                <option value="2">2 Persons</option>
+                <option value="3">3 Persons</option>
+                <option value="4">4 Persons</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="des-from !border-r-0">
+            <p>class</p>
+            <div className="flex flex-row">
+              <img src={classImg} alt="" />
+              <select
+                className="outline-none px-2 py-2 w-full"
+                name="ticketClass"
+                id="lws-ticketClass"
+                required
+              >
+                <option value="" hidden>
+                  Please Select
+                </option>
+                <option>Business</option>
+                <option>Economy</option>
+              </select>
+            </div>
+          </div>
+
+          <button className="addCity" type="submit" id="lws-addCity">
+            <svg
+              width="15px"
+              height="15px"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            <span className="text-sm">Book</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
