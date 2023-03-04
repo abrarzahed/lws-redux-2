ec71ef521861987a4bcf6db0791d1c4bd5b4e297
abrarzahed <abrarzahed6986@gmail.com>
2023-03-04 19:15:18 +0600

    created dynamic counter

---
+++ modules/module-5/vanilla-redux-toolkit/rtk/app/store.js
@@ -0,0 +1,12 @@
+const configureStore = require("@reduxjs/toolkit").configureStore;
+const counterReducer = require("../features/counter/counterSlice");
+const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice");
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    dynamicCounter: dynamicCounterReducer,
+  },
+});
+
+module.exports = store;
