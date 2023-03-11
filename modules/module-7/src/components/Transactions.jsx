f9214a187eff3ac162a50a509a4ecd4f10a3e9be
abrarzahed <abrarzahed6986@gmail.com>
2023-03-11 18:07:07 +0600

    completed listing transaction

--- modules/module-7/src/components/Transactions.jsx
+++ modules/module-7/src/components/Transactions.jsx
@@ -1,27 +1,52 @@
 import editIcon from "../assets/edit.svg";
 import deleteIcon from "../assets/delete.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchTransactionsAsync } from "../features/transaction/transactionSlice";
 
 export default function Transactions() {
+  const dispatch = useDispatch();
+  const { isLoading, isError, error, transactions } = useSelector(
+    (state) => state.transactions
+  );
+
+  useEffect(() => {
+    dispatch(fetchTransactionsAsync());
+  }, [dispatch]);
   return (
     <>
-      <p className="second_heading">Your Transactions:</p>
-
-      <div className="conatiner_of_list_of_transactions">
-        <ul>
-          <li className="transaction income">
-            <p>Earned this month</p>
-            <div className="right">
-              <p>৳ 100</p>
-              <button className="link">
-                <img className="icon" src={editIcon} alt="edit icon" />
-              </button>
-              <button className="link">
-                <img className="icon" src={deleteIcon} alt="delete icon" />
-              </button>
-            </div>
-          </li>
-        </ul>
-      </div>
+      {!isLoading && !isError && transactions.length > 0 ? (
+        <>
+          <p className="second_heading">Your Transactions:</p>
+          <div className="conatiner_of_list_of_transactions">
+            <ul>
+              {transactions.map((transaction) => (
+                <li
+                  key={transaction.id}
+                  className={`transaction ${transaction.type}`}
+                >
+                  <p>{transaction.name}</p>
+                  <div className="right">
+                    <p>৳ {transaction.amount}</p>
+                    <button className="link">
+                      <img className="icon" src={editIcon} alt="edit icon" />
+                    </button>
+                    <button className="link">
+                      <img
+                        className="icon"
+                        src={deleteIcon}
+                        alt="delete icon"
+                      />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <p className="error">{error}</p>
+      )}
     </>
   );
 }
