f46c15606f8a647e91ef48b9d38c7c2c731c0a8d
abrarzahed <abrarzahed6986@gmail.com>
2023-03-13 13:36:06 +0600

    created apis and slice

--- assignments/module-7-assignment/src/features/jobs/jobsApi.js
+++ assignments/module-7-assignment/src/features/jobs/jobsApi.js
@@ -4,3 +4,18 @@ export const getJobs = async () => {
   const response = await axiosInstance.get("/jobs");
   return response.data;
 };
+
+export const addJob = async (jobData) => {
+  const response = await axiosInstance.post("/jobs", jobData);
+  return response.data;
+};
+
+export const editJob = async (job) => {
+  const response = await axiosInstance.patch(`/jobs/${job.id}`, job);
+  return response.data;
+};
+
+export const deleteJob = async (id) => {
+  await axiosInstance.delete(`/jobs/${id}`);
+  return id;
+};
--- assignments/module-7-assignment/src/features/jobs/jobsSlice.js
+++ assignments/module-7-assignment/src/features/jobs/jobsSlice.js
@@ -15,6 +15,24 @@ export const getJobsAsync = createAsyncThunk("jobs/getJobs", async () => {
   return jobs;
 });
 
+export const addJobAsync = createAsyncThunk("jobs/addJob", async (jobData) => {
+  const job = getJobs(jobData);
+  return job;
+});
+
+export const editJobAsync = createAsyncThunk(
+  "jobs/editJob",
+  async (jobData) => {
+    const job = getJobs(jobData);
+    return job;
+  }
+);
+
+export const deleteJobAsync = createAsyncThunk("jobs/deleteJob", async (id) => {
+  const jobId = getJobs(id);
+  return jobId;
+});
+
 const jobsSlice = createSlice({
   name: "jobs",
   initialState,
@@ -37,6 +55,30 @@ const jobsSlice = createSlice({
         state.isError = true;
         state.error = action.error?.message;
         state.jobs = action.payload;
+      })
+      .addCase(addJobAsync.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = "";
+        state.jobs = [...state.jobs, action.payload];
+      })
+      .addCase(editJobAsync.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = "";
+        state.jobs = state.jobs.map((job) => {
+          if (job.id === action.payload.id) {
+            return action.payload;
+          } else {
+            return job;
+          }
+        });
+      })
+      .addCase(deleteJobAsync.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = "";
+        state.jobs = state.jobs.filter((job) => job.id !== action.payload);
       });
   },
 });
