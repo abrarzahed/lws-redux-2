17535bb192d03eda302d2451411b75fcd14f1c85
abrarzahed <abrarzahed6986@gmail.com>
2023-03-16 22:36:37 +0600

    configured reacr-dom

--- assignments/module-8-assignment/src/App.jsx
+++ assignments/module-8-assignment/src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import AddBook from "./pages/AddBook";
+import EditBook from "./pages/EditBook";
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addBook" element={<AddBook />} />
+        <Route path="/editBook/:bookId" element={<EditBook />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ assignments/module-8-assignment/src/components/FilterButtons.jsx
@@ -0,0 +1,3 @@
+export default function FilterButtons() {
+  return <div>FilterButtons</div>;
+}
