647ada53bd1d6755b68808db2ff85930a42cd1af
abrarzahed <abrarzahed6986@gmail.com>
2023-03-17 23:14:59 +0600

    completed filter feature

--- assignments/module-8-assignment/src/components/BookList.jsx
+++ assignments/module-8-assignment/src/components/BookList.jsx
@@ -1,8 +1,20 @@
+import { useSelector } from "react-redux";
 import { useGetBooksQuery } from "../features/api/apiSlice";
 import SingleBook from "./SingleBook";
 
 export default function BookList() {
   const { data: books, isLoading, isError, error } = useGetBooksQuery();
+
+  const { category } = useSelector((state) => state.filters);
+
+  // filter by category
+  const filterByCategory = (book) => {
+    if (category === "All") {
+      return true;
+    } else {
+      return book.featured;
+    }
+  };
 
   // decide what to render
   let content;
@@ -14,7 +26,9 @@ export default function BookList() {
     content = <div className="text-red-500">No books found</div>;
   }
   if (!isLoading && !isError && books?.length > 0) {
-    content = books.map((book) => <SingleBook key={book.id} book={book} />);
+    content = books
+      .filter(filterByCategory)
+      .map((book) => <SingleBook key={book.id} book={book} />);
   }
   return (
     <div className="space-y-6 md:space-y-0 md:grid grid-cols-1 lg:grid-cols-3 gap-6">
--- assignments/module-8-assignment/src/components/FilterButtons.jsx
+++ assignments/module-8-assignment/src/components/FilterButtons.jsx
@@ -1,8 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { categoryChanged } from "../features/filters/filterSlice";
+
 export default function FilterButtons() {
+  const dispatch = useDispatch();
+  const { category } = useSelector((state) => state.filters);
+
+  // handle filter change
+  const handleFilterChange = (category) => {
+    dispatch(categoryChanged(category));
+  };
   return (
     <div className="flex items-center space-x-4">
-      <button className="lws-filter-btn active-filter">All</button>
-      <button className="lws-filter-btn">Featured</button>
+      <button
+        className={`lws-filter-btn ${category === "All" && "active-filter"}`}
+        onClick={() => handleFilterChange("All")}
+      >
+        All
+      </button>
+      <button
+        className={`lws-filter-btn ${
+          category === "Featured" && "active-filter"
+        }`}
+        onClick={() => handleFilterChange("Featured")}
+      >
+        Featured
+      </button>
     </div>
   );
 }
