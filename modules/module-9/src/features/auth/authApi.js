0758e2d5d8534f6fa55b84efbd0ee654231ef450
abrarzahed <abrarzahed6986@gmail.com>
2023-03-19 16:49:48 +0600

    auth api and slice

--- modules/module-9/src/features/auth/authApi.js
+++ modules/module-9/src/features/auth/authApi.js
@@ -1,7 +1,64 @@
 import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => {
-    // endpoints go here
-  },
+   endpoints: (builder) => ({
+      // endpoints go here
+      register: builder.mutation({
+         query: (data) => ({
+            url: `/register`,
+            method: "POST",
+            body: data,
+         }),
+         async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+            try {
+               const result = await queryFulfilled;
+               localStorage.setItem(
+                  "lwsChatApp2Auth",
+                  JSON.stringify({
+                     accessToken: result.data.accessToken,
+                     user: result.data.user,
+                  })
+               );
+               dispatch(
+                  userLoggedIn({
+                     accessToken: result.data.accessToken,
+                     user: result.data.user,
+                  })
+               );
+            } catch (error) {
+               console.log(error);
+            }
+         },
+      }),
+      login: builder.mutation({
+         query: (data) => ({
+            url: `/login`,
+            method: "POST",
+            body: data,
+         }),
+         async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+            try {
+               const result = await queryFulfilled;
+               localStorage.setItem(
+                  "lwsChatApp2Auth",
+                  JSON.stringify({
+                     accessToken: result.data.accessToken,
+                     user: result.data.user,
+                  })
+               );
+               dispatch(
+                  userLoggedIn({
+                     accessToken: result.data.accessToken,
+                     user: result.data.user,
+                  })
+               );
+            } catch (error) {
+               console.log(error);
+            }
+         },
+      }),
+   }),
 });
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
