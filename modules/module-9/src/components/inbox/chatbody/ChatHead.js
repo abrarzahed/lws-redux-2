7c9470c26579115042aa9a73538a2191d8fdfe77
abrarzahed <abrarzahed6986@gmail.com>
2023-03-23 19:22:22 +0600

    completed chatHead: module 9

--- modules/module-9/src/components/inbox/chatbody/ChatHead.js
+++ modules/module-9/src/components/inbox/chatbody/ChatHead.js
@@ -1,12 +1,22 @@
-export default function ChatHead({ avatar, name }) {
-    return (
-        <div className="relative flex items-center p-3 border-b border-gray-300">
-            <img
-                className="object-cover w-10 h-10 rounded-full"
-                src={avatar}
-                alt={name}
-            />
-            <span className="block ml-2 font-bold text-gray-600">{name}</span>
-        </div>
-    );
+import { useSelector } from "react-redux";
+import gravatarUrl from "gravatar-url";
+
+export default function ChatHead({ message = {} }) {
+   const { email: authUserEmail } = useSelector((state) => state.auth);
+   const { email: otherUserEmail, name: otherUserName } = [
+      message.sender,
+      message.receiver,
+   ].find((person) => person.email !== authUserEmail);
+   return (
+      <div className="relative flex items-center p-3 border-b border-gray-300">
+         <img
+            className="object-cover w-10 h-10 rounded-full"
+            src={gravatarUrl(otherUserEmail)}
+            alt={otherUserName}
+         />
+         <span className="block ml-2 font-bold text-gray-600">
+            {otherUserName}
+         </span>
+      </div>
+   );
 }
