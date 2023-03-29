e272ad1ed1b0ed54c37005ea30e643b86d90e903
abrarzahed <abrarzahed6986@gmail.com>
2023-03-29 17:49:35 +0600

    React.memo

--- modules/module-11/src/components/WatchedVideos.js
+++ modules/module-11/src/components/WatchedVideos.js
@@ -2,19 +2,19 @@ import { useSelector } from "react-redux";
 import VideoItem from "./VideoItem";
 
 export default function WatchedVideos() {
-    const watchedVideos = useSelector((state) =>
-        state.videos.videos.filter((v) => v.watched)
-    );
+  const watchedVideos = useSelector((state) =>
+    state.videos.videos.filter((v) => v.watched)
+  );
 
-    console.log("[WatchedVideos] renders");
+  console.log("[WatchedVideos] renders");
 
-    return (
-        <div>
-            <ul className="divide-y divide-slate-200">
-                {watchedVideos.map((video) => (
-                    <VideoItem key={video.id} video={video} />
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul className="divide-y divide-slate-200">
+        {watchedVideos.map((video) => (
+          <VideoItem key={video.id} video={video} />
+        ))}
+      </ul>
+    </div>
+  );
 }
